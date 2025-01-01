f7cd58e6db013034d7244b8b80c269daf798e352
Romanch Roshan Singh <137044362+codedpool@users.noreply.github.com>
2025-01-01 23:56:57 +0530

    Time: 1 ms (99.88%), Space: 46.2 MB (39.25%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int num : nums) {
+            result ^= num; // XOR each number with result
+        }
+        return result; // The single number remains
+    }
+}
